362b287e2d21a77a450b42509a92c4c0695f0abd
avijit <avijitroy2944@gmail.com>
2024-01-25 22:21:02 +0530

    add

---
+++ src/api/report/sendMail-controller.js
@@ -0,0 +1,56 @@
+const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
+const mail = require("express").Router();
+
+const CLIENT_ID =
+  "406430181104-6gs5fpvocscoie2juea1mui7t26pkcmu.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URL = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04RGjZ6SwBjx5CgYIARAAGAQSNwF-L9IrzzfJTFj3ouLVvoXvjptXd-InDHZXxTQQsh1UrRyNycjYn2zbIK0YvXVZHb8-pXHbOmw";
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+mail.post("/send", async (req, res, next) => {
+  try {
+    const { user, subject, body } = req.body;
+
+    // Retrieve access token
+    const accessToken = await oauth2Client.getAccessToken();
+
+    // Create a nodemailer transport with OAuth2 authentication
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "ravijit512@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+
+    // Define mail options
+    const mailOptions = {
+      from: "NODE <ravijit512@gmail.com>",
+      to: user,
+      subject: subject,
+      text: body,
+    };
+
+    // Send the email
+    const result = await transport.sendMail(mailOptions);
+    res.send({ msg: "DONE!!" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = { mail };
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { gmr } = require("./api/report/meeting-controller");
+const { mail } = require("./api/report/sendMail-controller");
 
 // user APIs
 router.use("/user", require("./api/user/route"));
@@ -7,5 +8,6 @@ router.use("/client", require("./api/client/route"));
 router.use("/report", require("./api/report/route"));
 router.use("/notification", require("./api/notification/router"));
 router.use("/meet", gmr);
+router.use("/mail", mail);
 
 module.exports = router;
